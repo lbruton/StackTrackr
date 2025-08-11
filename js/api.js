6a5ed7649cd874b59a1d93246cb9df3b531c30af
Lonnie Bruton <lbruton@lonniebruton.com>
2025-08-11 15:42:36 -0500

    Show default provider button text

--- js/api.js
+++ js/api.js
@@ -418,7 +418,7 @@ const updateDefaultProviderButtons = () => {
     if (!btn) return;
     btn.classList.remove("default", "backup", "inactive");
     if (config.provider === prov && keys[prov]) {
-      btn.textContent = "";
+      btn.textContent = "Default";
       btn.classList.add("default");
     } else if (keys[prov]) {
       btn.textContent = "Backup";
